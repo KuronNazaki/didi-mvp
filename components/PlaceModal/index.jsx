0de113226c4a394300f8850b7370fcb7b9263c26
cnhh <cnhh85@gmail.com>
2023-06-02 00:08:50 +0700

    add generic use for modal

--- components/PlaceModal/index.jsx
+++ components/PlaceModal/index.jsx
@@ -1,38 +1,32 @@
 import { View, Pressable, Text, Image } from "react-native";
-import Pic from "../../assets/Pic.png";
 import Star from "../../assets/svg/star";
 import Location from "../../assets/svg/location";
 import Call from "../../assets/svg/call";
 import DocumentText from "../../assets/svg/documentText";
 import { TextInputWithLabel } from "../Input";
 
-const PlaceModal = () => {
+const PlaceModal = ({ image, title, description, rate, rateNumber }) => {
   return (
     <View className="h-screen w-screen z-10 absolute">
       <View className="h-full w-full bg-black opacity-80 absolute"></View>
       <View className="h-full w-full flex-col">
         <View className="mt-auto w-11/12 mx-auto z-50">
           <Image
-            source={Pic}
+            source={image}
             style={{ width: "100%" }}
             className="rounded-t-xl"
           />
 
           <View className="bg-white p-4">
             <View className="flex-row items-end h-8">
-              <Text className="text-[#55555e] text-3xl font-bold">
-                Thác Datanla
-              </Text>
+              <Text className="text-[#55555e] text-3xl font-bold">{title}</Text>
               <View className="ml-auto flex-row items-center">
                 <Star />
-                <Text className="text-[#FFCC00]">{4.5}/5 </Text>
-                <Text className="text-[#FFE580]">({402})</Text>
+                <Text className="text-[#FFCC00]">{rate}/5 </Text>
+                <Text className="text-[#FFE580]">({rateNumber})</Text>
               </View>
             </View>
-            <Text className="mt-3 text-[#8A8A8E]">
-              Relaxed pick serving spring rolls, pho, banh mi & other Vietnamese
-              staples, alongside beer.
-            </Text>
+            <Text className="mt-3 text-[#8A8A8E]">{description}</Text>
             <View className="flex-row items-center">
               <Location className="mt-8 mr-2" />
               <TextInputWithLabel placeholder="Address" />
